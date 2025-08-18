c3946bfeed18251fb936fd9534a79b7c2e8668e7
Todd OST <todd.e.parsons@gmail.com>
2025-08-18 09:15:12 +0100

    RF: Fix loops for JSON outputting

--- psychopy/experiment/loops.py
+++ psychopy/experiment/loops.py
@@ -132,18 +132,26 @@ class TrialHandler(_BaseLoopHandler):
         super(TrialHandler, self).__init__()
         self.type = 'TrialHandler'
         self.exp = exp
-        self.order = ['name']  # make name come first (others don't matter)
+        self.order = [
+            "name",
+            "loopType",
+            "isTrials",
+            "conditionsFile",
+            "nReps",
+            "Selected rows",
+            "random seed"
+        ]
         self.params = {}
         self.params['name'] = Param(
             name, valType='code', inputType="name", updates=None, allowedUpdates=None,
             label=_translate('Name'),
             hint=_translate("Name of this loop"))
         self.params['nReps'] = Param(
-            nReps, valType='num', inputType="spin", updates=None, allowedUpdates=None,
+            nReps, valType='code', inputType="single", updates=None, allowedUpdates=None,
             label=_translate('Num. repeats'),
             hint=_translate("Number of repeats (for each condition)"))
         self.params['conditions'] = Param(
-            list(conditions), valType='str', inputType="single",
+            list(conditions), valType='str', inputType="hidden",
             updates=None, allowedUpdates=None,
             label=_translate('Conditions'),
             hint=_translate("A list of dictionaries describing the "
@@ -160,12 +168,12 @@ class TrialHandler(_BaseLoopHandler):
             }
         )
         self.params['endPoints'] = Param(
-            list(endPoints), valType='num', inputType="single", updates=None, allowedUpdates=None,
+            list(endPoints), valType='code', inputType="hidden", updates=None, allowedUpdates=None,
             label=_translate('End points'),
             hint=_translate("The start and end of the loop (see flow "
                             "timeline)"))
         self.params['Selected rows'] = Param(
-            selectedRows, valType='str', inputType="single",
+            selectedRows, valType='code', inputType="single",
             updates=None, allowedUpdates=None,
             label=_translate('Selected rows'),
             hint=_translate("Select just a subset of rows from your condition"
@@ -174,8 +182,7 @@ class TrialHandler(_BaseLoopHandler):
         # NB staircase is added for the sake of the loop properties dialog:
         self.params['loopType'] = Param(
             loopType, valType='str', inputType="choice",
-            allowedVals=['random', 'sequential', 'fullRandom',
-                         'staircase', 'interleaved staircases'],
+            allowedVals=['random', 'sequential', 'fullRandom'],
             label=_translate('Loop type'),
             hint=_translate("How should the next condition value(s) be "
                             "chosen?"))
@@ -517,7 +524,19 @@ class StairHandler(_BaseLoopHandler):
         super(StairHandler, self).__init__()
         self.type = 'StairHandler'
         self.exp = exp
-        self.order = ['name']  # make name come first (others don't matter)
+        self.order = [
+            "name",
+            "isTrials",
+            "nReps",
+            "start value",
+            "max value",
+            "min value",
+            "step sizes",
+            "step type",
+            "N up",
+            "N down",
+            "N reversals"
+        ]
         self.children = []
         self.params = {}
         self.params['name'] = Param(
@@ -525,61 +544,48 @@ class StairHandler(_BaseLoopHandler):
             hint=_translate("Name of this loop"),
             label=_translate('Name'))
         self.params['nReps'] = Param(
-            nReps, valType='num', inputType='spin',
+            nReps, valType='code', inputType='single',
             label=_translate('nReps'),
             hint=_translate("(Minimum) number of trials in the staircase"))
         self.params['start value'] = Param(
-            startVal, valType='num', inputType='single',
+            startVal, valType='code', inputType='single', categ="Staircase",
             label=_translate('Start value'),
             hint=_translate("The initial value of the parameter"))
         self.params['max value'] = Param(
-            maxVal, valType='num', inputType='single',
+            maxVal, valType='code', inputType='single', categ="Staircase",
             label=_translate('Max value'),
             hint=_translate("The maximum value the parameter can take"))
         self.params['min value'] = Param(
-            minVal, valType='num', inputType='single',
+            minVal, valType='code', inputType='single', categ="Staircase",
             label=_translate('Min value'),
             hint=_translate("The minimum value the parameter can take"))
         self.params['step sizes'] = Param(
-            stepSizes, valType='list', inputType='single',
+            stepSizes, valType='list', inputType='single', categ="Staircase",
             label=_translate('Step sizes'),
             hint=_translate("The size of the jump at each step (can change"
                             " on each 'reversal')"))
         self.params['step type'] = Param(
-            stepType, valType='str', inputType='choice', allowedVals=['lin', 'log', 'db'],
+            stepType, valType='str', inputType='choice', categ="Staircase",
+            allowedVals=['lin', 'log', 'db'],
             label=_translate('Step type'),
             hint=_translate("The units of the step size (e.g. 'linear' will"
                             " add/subtract that value each step, whereas "
                             "'log' will ad that many log units)"))
         self.params['N up'] = Param(
-            nUp, valType='num', inputType='spin',
+            nUp, valType='code', inputType='single', categ="Staircase",
             label=_translate('N up'),
             hint=_translate("The number of 'incorrect' answers before the "
                             "value goes up"))
         self.params['N down'] = Param(
-            nDown, valType='num', inputType='spin',
+            nDown, valType='code', inputType='single', categ="Staircase",
             label=_translate('N down'),
             hint=_translate("The number of 'correct' answers before the "
                             "value goes down"))
         self.params['N reversals'] = Param(
-            nReversals, valType='num', inputType='spin',
+            nReversals, valType='code', inputType='single', categ="Staircase",
             label=_translate('N reversals'),
             hint=_translate("Minimum number of times the staircase must "
                             "change direction before ending"))
-        # these two are really just for making the dialog easier (they won't
-        # be used to generate code)
-        self.params['loopType'] = Param(
-            'staircase', valType='str', inputType='choice',
-            allowedVals=['random', 'sequential', 'fullRandom', 'staircase',
-                         'interleaved staircases'],
-            label=_translate('Loop type'),
-            hint=_translate("How should the next trial value(s) be chosen?"))
-        # NB this is added for the sake of the loop properties dialog
-        self.params['endPoints'] = Param(
-            list(endPoints), valType='num', inputType='spin',
-            label=_translate('End points'),
-            hint=_translate('Where to loop from and to (see values currently'
-                            ' shown in the flow view)'))
         self.params['isTrials'] = Param(
             isTrials, valType='bool', inputType='bool', updates=None, allowedUpdates=None,
             label=_translate("Is trials"),
@@ -685,8 +691,16 @@ class MultiStairHandler(_BaseLoopHandler):
         super(MultiStairHandler, self).__init__()
         self.type = 'MultiStairHandler'
         self.exp = exp
-        self.order = ['name']  # make name come first
+        self.order = [
+            "name",
+            "isTrials",
+            "nReps",
+            "stairType",
+            "switchMethod",
+            "conditionsFile"
+        ]
         self.params = {}
+        self.depends = []
         self.params['name'] = Param(
             name, valType='code', inputType='name',
             label=_translate('Name'),
@@ -705,56 +719,37 @@ class MultiStairHandler(_BaseLoopHandler):
             allowedVals=['random', 'sequential', 'fullRandom'],
             label=_translate('Switch method'),
             hint=_translate("How to select the next staircase to run"))
-        # these two are really just for making the dialog easier (they won't
-        # be used to generate code)
-        self.params['loopType'] = Param(
-            'staircase', valType='str', inputType='choice',
-            allowedVals=['random', 'sequential', 'fullRandom', 'staircase',
-                         'interleaved staircases'],
-            label=_translate('Loop type'),
-            hint=_translate("How should the next trial value(s) be chosen?"))
-        self.params['endPoints'] = Param(
-            list(endPoints), valType='num', inputType='spin',
-            label=_translate('End points'),
-            hint=_translate('Where to loop from and to (see values currently'
-                            ' shown in the flow view)'))
         self.params['conditions'] = Param(
-            list(conditions), valType='list', inputType='conditions',
+            list(conditions), valType='list', inputType='hidden',
             updates=None, allowedUpdates=None,
             label=_translate('Conditions'),
             hint=_translate("A list of dictionaries describing the "
-                            "differences between each staircase"))
+                            "differences between each staircase")
+        ) 
 
-        def getTemplate():
-            """
-            Method to get the template for this loop's chosen stair type. This is specified as a
-            method rather than a simple value as the control needs to update its target according
-            to the current value of stairType.
-
-            Returns
-            -------
-            pathlib.Path
-                Path to the appropriate template file
-            """
-            # root folder
-            root = Path(__file__).parent
-            # get file path according to stairType param
-            if self.params['stairType'] == "QUEST":
-                return root / "questTemplate.xltx"
-            elif self.params['stairType'] == "questplus":
-                return root / "questPlusTemplate.xltx"
-            else:
-                return root / "staircaseTemplate.xltx"
-
-        self.params['conditionsFile'] = Param(
-            conditionsFile, valType='file', inputType='conditions', updates=None, allowedUpdates=None,
-            label=_translate('Conditions'),
-            hint=_translate("An xlsx or csv file specifying the parameters "
-                            "for each condition"),
-            ctrlParams={
-                'template': getTemplate
-            }
-        )
+        for stairType, template in [
+            ("simple", Path(__file__).parent / "staircaseTemplate.xltx"),
+            ("QUEST", Path(__file__).parent / "questTemplate.xltx"),
+            ("questplus", Path(__file__).parent / "questPlusTemplate.xltx"),
+        ]:
+            # create a param for this stair type
+            self.params[f'conditionsFile_{stairType}'] = Param(
+                conditionsFile, valType='file', inputType='conditions', updates=None, allowedUpdates=None,
+                label=_translate('Conditions'),
+                hint=_translate("An xlsx or csv file specifying the parameters "
+                                "for each condition"),
+                ctrlParams={
+                    'template': template
+                }
+            )
+            # make it dependent on stairType
+            self.depends.append({
+                "dependsOn": "stairType",
+                "condition": f"=='{stairType}'",
+                "param": f'conditionsFile_{stairType}',
+                "true": "show",  # what to do with param if condition is True
+                "false": "hide",  # permitted: hide, show, enable, disable
+            })
         self.params['isTrials'] = Param(
             isTrials, valType='bool', inputType='bool', updates=None, allowedUpdates=None,
             label=_translate("Is trials"),
